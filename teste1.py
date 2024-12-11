6383fcfb2d3e40e274ba6dc26856656c60b0e327
vinicius sposito <167882525+nogsposito@users.noreply.github.com>
2024-12-11 18:29:58 -0300

    Delete teste1.py

--- teste1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = {'Nome': ['Alice', 'Bob', None, 'Vini'],
-        'Idade': [25, None, None, 22],
-        'Salario': [5000, 2400, None, 4500]}
-
-df = pd.DataFrame(data)
-print(df.isnull())
-print(df.isnull().sum()) 
